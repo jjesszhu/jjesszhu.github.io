5d4fdd700ec39e22fdd1800d0e7b2545fe1661ce
Jessica Zhu <jessica.jz.zhu@gmail.com>
2023-09-01 02:36:41 -0400

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,17 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon.ico"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
           integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+          crossOrigin="anonymous"
         />
       </head>
       <body>{children}</body>
