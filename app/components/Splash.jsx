c70e7c8af8a0928bad2752e924f5491fa349fefb
Jessica Zhu <jessica.jz.zhu@gmail.com>
2024-05-06 16:14:35 -0400

    remove resume and backsplash

--- app/components/Splash.jsx
+++ app/components/Splash.jsx
@@ -6,14 +6,6 @@ const Splash = ({font}) => {
     <header className={styles.splashBackground}>
       <h2 className={styles.splashSmText}>a website about</h2>
       <h1 className={styles.splashBigText}>JESSICA ZHU</h1>
-      <a
-        className={styles.splashLink}
-        href="https://drive.google.com/file/d/13HK4uhs_zX3s-wapW2KRQ7auIeEBmTJG/view?usp=sharing"
-        rel="noreferrer"
-        target="_blank"
-      >
-        résumé
-      </a>
     </header>
   );
 };
